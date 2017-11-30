a31469bc1c58888be29f08630c258ba648ec596f
dylanP3495 <palafoxdylan@gmail.com>
2017-11-30 14:21:15 -0800

    Initial commit

---
+++ arraysOfTheSecondDimension.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class arraysOfTheSecondDimension here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class arraysOfTheSecondDimension
+{
+    int[] array;
+    int[][] twoDimensionalArray;
+    int[][][][][][][][][][] tenD;
+    public arraysOfTheSecondDimension(){
+        array = new int[10];
+        twoDimensionalArray = new int [5][10];
+        // Two dimensional  Arrays are ALWAYS in 
+        //row-major format.
+        for (int row = 0; row < twoDimensionalArray.length; row++){
+            for (int col = 0; col < twoDimensionalArray[0].length; col++){
+                
+            }
+        }
+    }
+}
